08a508967df6858558d97aca1ef3b042b44a0f9a
Mathieu PASCO-BREILLOT <mathieu.pasco@imie-rennes.fr>
2016-01-14 16:51:00 +0100

    Change an assets name. Add the new class and manager Stones.

--- PokeForeverIHM/PokeForeverIHM/UsersControls/StonesManager.xaml.cs
+++ PokeForeverIHM/PokeForeverIHM/UsersControls/StonesManager.xaml.cs
@@ -1,37 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using System.Collections.ObjectModel;
+using PokeForeverIHM.Class;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace PokeForeverIHM.UsersControls
 {
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
 	public sealed partial class StonesManager : UserControl
 	{
+		private int ballToSwitchOut = -1;
+		private int ballToSwitchIn = -1;
+		private ObservableCollection<Stone> stones = Stone.Stones;
 		public StonesManager()
 		{
 			this.InitializeComponent();
+			DataContext = stones;
 		}
 
 		private void StonesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+			if (ballToSwitchIn == -1)
+			{
+				if (ballToSwitchOut == -1)
+				{
+					ballToSwitchOut = StonesListView.SelectedIndex;
+				}
+				else
+				{
+					ballToSwitchIn = StonesListView.SelectedIndex;
 
-		}
+					Stone pokemonTempOut = stones[ballToSwitchOut];
+					Stone pokemonTempIn = stones[ballToSwitchIn];
 
-		private void StonesListView_DragItemsCompleted(ListViewBase sender, DragItemsCompletedEventArgs args)
-		{
+					stones.Remove(pokemonTempOut);
+					stones.Remove(pokemonTempIn);
 
+					if (ballToSwitchIn > ballToSwitchOut)
+					{
+						stones.Insert(ballToSwitchOut, pokemonTempIn);
+						stones.Insert(ballToSwitchIn, pokemonTempOut);
+					}
+					else
+					{
+						stones.Insert(ballToSwitchIn, pokemonTempOut);
+						stones.Insert(ballToSwitchOut, pokemonTempIn);
+					}
+					ballToSwitchIn = -1;
+					ballToSwitchOut = -1;
+				}
+			}
 		}
 	}
 }
