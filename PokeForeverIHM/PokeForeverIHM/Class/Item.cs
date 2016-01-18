70bc4f49d64da95d2212149c0524a9fd05392457
Mathieu PASCO-BREILLOT <mathieu.pasco@imie-rennes.fr>
2016-01-18 22:51:10 +0100

    Renamed the CommonObjectManager to ItemManager and add the code behind correctly.

---
+++ PokeForeverIHM/PokeForeverIHM/Class/Item.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace PokeForeverIHM.Class
+{
+	public class Item
+	{
+		private string name;
+		private ushort quantity, price;
+		private ImageSource image;
+		private static ObservableCollection<Item> instance;
+
+		public ushort Quantity
+		{
+			get
+			{
+				return quantity;
+			}
+
+			set
+			{
+				quantity = value;
+			}
+		}
+
+		public string SetImage
+		{
+			set
+			{
+				image = new BitmapImage(new Uri(value));
+			}
+		}
+
+		public ImageSource GetImage
+		{
+			get
+			{
+				return image;
+			}
+		}
+
+		public static ObservableCollection<Item> Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new ObservableCollection<Item>();
+					instance.Add(new Item("Pokebike"));
+					instance.Add(new Item("Pokeflute"));
+					instance.Add(new Item("FishingRod"));
+					instance.Add(new Item("Potion"));
+					instance.Add(new Item("Antidote"));
+				}
+				return instance;
+			}
+		}
+
+		public Item(string name)
+		{
+			SetImage = $"ms-appx:///Assets/Items/{name}.png";
+			Quantity = 0;
+		}
+	}
+}
---
+++ PokeForeverIHM/PokeForeverIHM/UsersControls/ItemManager.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using PokeForeverIHM.Class;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PokeForeverIHM.UsersControls
+{
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public sealed partial class ItemManager : UserControl
+	{
+		private int itemToSwitchOut = -1;
+		private int itemToSwitchIn = -1;
+		private ObservableCollection<Item> items= Item.Instance;
+		public ItemManager()
+		{
+			this.InitializeComponent();
+			DataContext = items;
+		}
+
+		private void ItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (itemToSwitchIn == -1)
+			{
+				if (itemToSwitchOut == -1)
+				{
+					itemToSwitchOut = ItemListView.SelectedIndex;
+				}
+				else
+				{
+					itemToSwitchIn = ItemListView.SelectedIndex;
+
+					Item itemTempOut = items[itemToSwitchOut];
+					Item itemTempIn = items[itemToSwitchIn];
+
+					items.Remove(itemTempOut);
+					items.Remove(itemTempIn);
+
+					if (itemToSwitchIn > itemToSwitchOut)
+					{
+						items.Insert(itemToSwitchOut, itemTempIn);
+						items.Insert(itemToSwitchIn, itemTempOut);
+					}
+					else
+					{
+						items.Insert(itemToSwitchIn, itemTempOut);
+						items.Insert(itemToSwitchOut, itemTempIn);
+					}
+					itemToSwitchIn = -1;
+					itemToSwitchOut = -1;
+				}
+			}
+		}
+	}
+}
